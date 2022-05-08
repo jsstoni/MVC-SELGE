a89a4b2117481aae9870a4e4bf3817ff9dcded41
jsstoni <jsstoniha@gmail.com>
2022-05-08 17:38:08 -0400

    ruta real

--- index.php
+++ index.php
@@ -4,3 +4,5 @@ header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 header("Content-Type: application/json");
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', realpath(dirname(__FILE__)).DS);
