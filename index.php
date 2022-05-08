64a4487a34b9bfa6e65293b9147cfe59e0443e20
jsstoni <jsstoniha@gmail.com>
2022-05-08 17:36:52 -0400

    header control

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php
 session_start();
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+header("Content-Type: application/json");
