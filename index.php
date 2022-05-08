e7b7de11bcffcac5befa0ac42134700158bcd27d
jsstoni <jsstoniha@gmail.com>
2022-05-08 17:41:35 -0400

    autoloader

--- index.php
+++ index.php
@@ -6,3 +6,14 @@ header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 header("Content-Type: application/json");
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', realpath(dirname(__FILE__)).DS);
+function autoloading()
+{
+    spl_autoload_register(function ($class) {
+        $file = str_replace('\\', DS, $class).'.php';
+        if (file_exists($file)) {
+            require_once $file;
+            return;
+        }
+    });
+}
+autoloading();
